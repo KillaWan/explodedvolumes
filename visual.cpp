86cc2e6a46c238937f9a3fe98b5ec421d1767d85
Zitong <zitong.di.2909@gmail.com>
2026-05-24 22:21:04 +0200

    change ignore and axis color

--- visual.cpp
+++ visual.cpp
@@ -91,7 +91,7 @@ void main() {
 out vec4 FragColor;
 
 void main() {
-    FragColor = vec4(0.0, 0.0, 0.0, 1.0); // Black color for the axis
+    FragColor = vec4(1.0, 0.5, 0.0, 1.0); // orange color for the axis
 }
 )glsl";
 
